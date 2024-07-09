0e7bb6877cbc986d7b4b9f87c6dd87cbce0caec8
JuampiLeoz <mdqalumno@gmail.com>
2024-07-09 20:30:32 -0300

    Update config.py

--- componentes/config.py
+++ componentes/config.py
@@ -1,9 +1,9 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/plopparty'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
-    MAIL_USERNAME = 'consultasplopparty@gmail.com'
-    MAIL_PASSWORD = 'lhkd vtqw iahl ckiy'
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
